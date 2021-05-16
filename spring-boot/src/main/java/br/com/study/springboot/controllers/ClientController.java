fc1960de727d371a0f181a793ef383c10795ac60
Matheus Oliveira <matheusalmeida.soliveira@gmail.com>
2021-05-16 11:45:30 -0300

    Path Variable

--- spring-boot/src/main/java/br/com/study/springboot/controllers/ClientController.java
+++ spring-boot/src/main/java/br/com/study/springboot/controllers/ClientController.java
@@ -1,15 +1,23 @@
 package br.com.study.springboot.controllers;
 
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
 import br.com.study.springboot.models.Client;
 
 @RestController
+@RequestMapping("clients")
 public class ClientController {
 	
-	@GetMapping(path = "/client")
+	@GetMapping(path = "/any")
 	public Client getClient() {
 		return new Client(28, "Matheus", "123.456.789-10");
+	}
+	
+	@GetMapping(path = "/{id}")
+	public Client getClient(@PathVariable int id) {
+		return new Client(id, "Matheus", "123.456.789-10");
 	}
 }
