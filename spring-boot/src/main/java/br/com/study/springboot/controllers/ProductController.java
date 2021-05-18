406660b941980d2c86a85aef1e10c2771b5663ab
Matheus Oliveira <matheusalmeida.soliveira@gmail.com>
2021-05-18 18:50:34 -0300

    Get Products

--- spring-boot/src/main/java/br/com/study/springboot/controllers/ProductController.java
+++ spring-boot/src/main/java/br/com/study/springboot/controllers/ProductController.java
@@ -1,7 +1,10 @@
 package br.com.study.springboot.controllers;
 
+import java.util.ArrayList;
+
 import javax.validation.Valid;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
@@ -20,5 +23,10 @@ public class ProductController {
 	public Product newProduct(@Valid Product product) {
 		productRepository.save(product);
 		return product;
+	}
+	
+	@GetMapping
+	public Iterable<Product> getProducts() {
+		return productRepository.findAll();
 	}
 }
