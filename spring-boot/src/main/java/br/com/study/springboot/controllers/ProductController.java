c71d4b73b61a3f5e387f0e5243470af5190884c6
Matheus Oliveira <matheusalmeida.soliveira@gmail.com>
2021-05-18 18:54:57 -0300

    GetById

--- spring-boot/src/main/java/br/com/study/springboot/controllers/ProductController.java
+++ spring-boot/src/main/java/br/com/study/springboot/controllers/ProductController.java
@@ -1,10 +1,13 @@
 package br.com.study.springboot.controllers;
 
 import java.util.ArrayList;
+import java.util.Optional;
 
 import javax.validation.Valid;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.data.repository.query.Param;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
@@ -29,4 +32,10 @@ public class ProductController {
 	public Iterable<Product> getProducts() {
 		return productRepository.findAll();
 	}
+	
+	@GetMapping(path="/{id}")
+	public Optional<Product> getProductById(@PathVariable int id) {
+		return productRepository.findById(id);
+	}
+	
 }
