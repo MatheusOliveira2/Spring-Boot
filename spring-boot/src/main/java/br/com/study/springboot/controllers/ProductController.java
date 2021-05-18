b9f2e3bc21b8df3679b2f6acdcf28067b1898291
Matheus Oliveira <matheusalmeida.soliveira@gmail.com>
2021-05-17 21:37:21 -0300

    Saving Product

--- spring-boot/src/main/java/br/com/study/springboot/controllers/ProductController.java
+++ spring-boot/src/main/java/br/com/study/springboot/controllers/ProductController.java
@@ -1,19 +1,25 @@
 package br.com.study.springboot.controllers;
 
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.bind.annotation.RestController;
 
 import br.com.study.springboot.model.entities.Product;
+import br.com.study.springboot.model.repositories.ProductRepository;
 
 @RestController
 @RequestMapping("/api/products")
 public class ProductController {
+	
+	@Autowired
+	private ProductRepository productRepository;
 
 	@PostMapping
 	public Product newProduct(@RequestParam String name) {
 		Product product = new Product(name);
+		productRepository.save(product);
 		return product;
 	}
 }
