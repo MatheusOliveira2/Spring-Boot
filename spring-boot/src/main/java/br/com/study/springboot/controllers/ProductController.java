d4a8a37cb4216ec315de1fb23db91d6dcff73c47
Matheus Oliveira <matheusalmeida.soliveira@gmail.com>
2021-05-19 22:05:59 -0300

    Pagination

--- spring-boot/src/main/java/br/com/study/springboot/controllers/ProductController.java
+++ spring-boot/src/main/java/br/com/study/springboot/controllers/ProductController.java
@@ -4,6 +4,8 @@ import java.util.Optional;
 
 import javax.validation.Valid;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.data.domain.PageRequest;
+import org.springframework.data.domain.Pageable;
 import org.springframework.web.bind.annotation.DeleteMapping;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
@@ -30,6 +32,12 @@ public class ProductController {
 	@GetMapping
 	public Iterable<Product> getProducts() {
 		return productRepository.findAll();
+	}
+	
+	@GetMapping(path="/page/{pageNumber}")
+	public Iterable<Product> getProducts(@PathVariable int pageNumber) {
+		Pageable page = PageRequest.of(pageNumber,3);
+		return productRepository.findAll(page);
 	}
 	
 	@GetMapping(path="/{id}")
--- spring-boot/src/main/java/br/com/study/springboot/model/repositories/ProductRepository.java
+++ spring-boot/src/main/java/br/com/study/springboot/model/repositories/ProductRepository.java
@@ -1,10 +1,10 @@
 package br.com.study.springboot.model.repositories;
 
-import org.springframework.data.repository.CrudRepository;
-
+//import org.springframework.data.repository.CrudRepository;
+import org.springframework.data.repository.PagingAndSortingRepository;
 import br.com.study.springboot.model.entities.Product;
 
-public interface ProductRepository extends CrudRepository<Product, Integer>  {
-
+//public interface ProductRepository extends CrudRepository<Product, Integer>  {
+public interface ProductRepository extends PagingAndSortingRepository<Product, Integer> {
 	
 }
